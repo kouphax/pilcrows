38fc6ec1a3c784198769200077d806d84e3fde2e
James Hughes <james@yobriefca.se>
2014-07-06 10:54:27 +0100

    02 The Basics
    By this stage we have a very bare bones application
    
    1. A controller that responds to some of our "read-only" routes using
    the built in TODO helper
    2. A set of routes that wires these actions up to the routes

---
+++ java/app/controllers/Pilcrows.java
@@ -0,0 +1,23 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class Pilcrows extends Controller {
+
+    public static Result index() {
+        return TODO;
+    }
+
+    public static Result create() {
+        return TODO;
+    }
+
+    public static Result search() {
+        return TODO;
+    }
+    
+    public static Result edit() {
+        return TODO;
+    }
+}
---
+++ scala/app/controllers/Pilcrows.scala
@@ -0,0 +1,10 @@
+package controllers
+
+import play.api.mvc.Controller
+
+object Pilcrows extends Controller {
+  def index = TODO
+  def create = TODO
+  def search = TODO
+  def edit = TODO
+}
